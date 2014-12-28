2cec237cdb938524b54f1beede4dba8999dec0a6
Avijeet <avijeetsm@live.com>
2014-12-28 03:19:43 -0500

    Added Buy feature

--- public/buy.php
+++ public/buy.php
@@ -13,15 +13,38 @@
         }
         else
         {
-            $stock = lookup($_POST["symbol"]);
-            if ($stock === false)
+            $stockname = strtoupper($_POST["symbol"]);
+            $stock = lookup($stockname);
+            if (!(preg_match("/^\d+$/", $_POST["quantity"])))
+            {
+                apologize("Please enter positive integers only");
+            }
+            else if ($stock === false)
             {
                 apologize("Enter valid stock symbol");
             }
             else
             {
-                render("quote_result.php", ["title" => "Quote", "symbol" => $stock["symbol"], "name" => $stock["name"], "price" => $stock["price"]]);
-
+                $user = query("SELECT username, cash FROM users WHERE id = ?", $_SESSION["id"]);
+                $cash = $user[0]["cash"];
+                if ($cash < ($_POST["quantity"] * $stock["price"]))
+                {
+                    apologize("Ooops! Not enough money");
+                }
+                else
+                {
+                    $buysuccess = query("INSERT INTO stocks_owned (id, symbol, shares) VALUES(?, ?, ?) ON DUPLICATE KEY UPDATE shares = shares + VALUES(shares)",$_SESSION["id"], $stockname, $_POST["quantity"]);
+                    if ($buysuccess === false)
+                    {
+                        apologize("Buying failed. Please try again later");
+                    }
+                    else
+                    {
+                        query("UPDATE users SET cash=cash-? WHERE id=?",($_POST["quantity"] * $stock["price"]),$_SESSION["id"]);
+                    }
+                    redirect("index.php");
+                    
+                }
             }
         }
     }
